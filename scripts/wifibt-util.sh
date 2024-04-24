64a8e0371da18916a7bc2e758a9f8e2419b9f6f3
Jeffy Chen <jeffy.chen@rock-chips.com>
2024-04-24 17:27:50 +0800

    wifibt-util.sh: Add a few chips
    Also, sort the chip list alphabetically.
    
    Change-Id: I1f9c05cb862060142e3cceb4322f8500a50b0c03
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- scripts/wifibt-util.sh
+++ scripts/wifibt-util.sh
@@ -87,34 +87,39 @@ if [ ! -r "$CHIPS_FILE" ]; then
 
 	# Append built-in chip list
 	cat << EOF >> "$CHIPS_FILE"
-Realtek	RTL8188EU	0bda:8179	RTL8188EU.ko
 Realtek	RTL8188EU	0bda:0179	RTL8188EU.ko
-Realtek	RTL8723BU	0bda:b720	RTL8723BU.ko
-Realtek	RTL8723BS	024c:b723	RTL8723BS.ko
-Realtek	RTL8822BS	024c:b822	RTL8822BS.ko
-Realtek	RTL8723CS	024c:b703	RTL8723CS.ko
-Realtek	RTL8723DS	024c:d723	RTL8723DS.ko
+Realtek	RTL8188EU	0bda:8179	RTL8188EU.ko
 Realtek	RTL8188FU	0bda:f179	RTL8188FU.ko
-Realtek	RTL8822BU	0bda:b82c	RTL8822BU.ko
 Realtek	RTL8189ES	024c:8179	RTL8189ES.ko
 Realtek	RTL8189FS	024c:f179	RTL8189FS.ko
 Realtek	RTL8192DU	0bda:8194	RTL8192DU.ko
+Realtek	RTL8192EU	0bda:818b	RTL8192EU.ko
+Realtek	RTL8723BS	024c:b723	RTL8723BS.ko
+Realtek	RTL8723BU	0bda:b720	RTL8723BU.ko
+Realtek	RTL8723CS	024c:b703	RTL8723CS.ko
+Realtek	RTL8723DS	024c:d723	RTL8723DS.ko
 Realtek	RTL8812AU	0bda:8812	RTL8812AU.ko
+Realtek	RTL8821CS	024c:c821	RTL8821CS.ko
 Realtek	RTL8821CS	0bda:c821	RTL8821CS.ko
-Realtek	RTL8822CS	0bda:c822	RTL8822CS.ko
 Realtek	RTL8822BE	10ec:b822	RTL8822BE.ko
+Realtek	RTL8822BS	024c:b822	RTL8822BS.ko
+Realtek	RTL8822BU	0bda:b82c	RTL8822BU.ko
+Realtek	RTL8822CE	0bda:c123	RTL8822CE.ko
 Realtek	RTL8822CE	10ec:c822	RTL8822CE.ko
+Realtek	RTL8822CS	024c:c822	RTL8822CS.ko
+Realtek	RTL8822CS	0bda:c822	RTL8822CS.ko
 Realtek	RTL8852BE	10ec:b852	RTL8852BE.ko
+Broadcom	AP6212A	02d0:a9a6	bcmdhd.ko
+Broadcom	AP625X	02d0:a9bf	bcmdhd.ko	# AP6255/AP6256/AP6745
+Broadcom	AP6275P	14e4:449d	bcmdhd_pcie.ko
+Broadcom	AP6275S	02d0:aae8	bcmdhd.ko
+Broadcom	AP6276P	14e4:44a0	bcmdhd_pcie.ko
+Broadcom	AP6276S	02d0:aaec	bcmdhd.ko
 Broadcom	AP6330	02d0:4330	bcmdhd.ko
 Broadcom	AP6335	02d0:4335	bcmdhd.ko
 Broadcom	AP6354	02d0:4354	bcmdhd.ko
 Broadcom	AP6356S	02d0:4356	bcmdhd.ko
 Broadcom	AP6398S	02d0:4359	bcmdhd.ko
-Broadcom	AP6212A	02d0:a9a6	bcmdhd.ko
-Broadcom	AP625X	02d0:a9bf	bcmdhd.ko	# AP6255/AP6256/AP6745
-Broadcom	AP6275S	02d0:aae8	bcmdhd.ko
-Broadcom	AP6275P	14e4:449d	bcmdhd_pcie.ko
-Broadcom	AP6276P	14e4:44a0	bcmdhd_pcie.ko
 EOF
 fi
 
